c9df2e87ad7517d58c75d16b0175e4b8e549acc0
MikeWarren2014 <miwarren@iupui.edu>
2016-12-26 03:07:04 -0500

    Deleted phpMyAdmin
    Deleted phpMyAdmin dependency, as it was not necessary to run my
    project, contained sensitive information, and caused GitHub to think
    this was a PHP project.

--- var/www/html/db_test.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	$link = mysqli_connect('group-inventory-management-db.cu8hvhstcity.us-west-2.rds.amazonaws.com:3306', 'HeadAdmin', 'group6966e7665e746f727961646d696e', 'information_schema', 3306); 
-	var_dump($link);
-?>
