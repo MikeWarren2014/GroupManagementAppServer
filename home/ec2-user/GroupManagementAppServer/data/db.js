bd90fcb75dc2c19a40ca16ac1f19d79a7ba20409
MikeWarren2014 <mwarren04011990@gmail.com>
2016-12-26 03:52:11 -0500

    Update db.js

--- home/ec2-user/GroupManagementAppServer/data/db.js
+++ home/ec2-user/GroupManagementAppServer/data/db.js
@@ -3,10 +3,7 @@ var mysql = require('mysql');
 exports.db = function(callback)
 {
 	var c = mysql.createConnection({
-		host: "group-inventory-management-db.cu8hvhstcity.us-west-2.rds.amazonaws.com",
-		database: "group_inventory_management_db",
-		user: "HeadAdmin", 
-		password: "group696e76656e746f727961646d696e"
+		
 	});
 	callback(null, c);
 }
