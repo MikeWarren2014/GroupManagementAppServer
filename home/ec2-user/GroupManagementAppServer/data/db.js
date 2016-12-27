b26e9acd5ffbd8762ad3e7543feb0cc4deea47dd
MikeWarren2014 <miwarren@iupui.edu>
2016-12-27 01:29:31 -0500

    credential clean-up
    Removed private key, certificate and unnecessary BASH file. Also changed
    database credentials to some default credentials for maintainers of this
    project.

--- home/ec2-user/GroupManagementAppServer/data/db.js
+++ home/ec2-user/GroupManagementAppServer/data/db.js
@@ -3,7 +3,9 @@ var mysql = require('mysql');
 exports.db = function(callback)
 {
 	var c = mysql.createConnection({
-		
+		host    : "localhost",
+		database: "group_inventory_management_db",
+		user    : "root"
 	});
 	callback(null, c);
 }
--- home/ec2-user/test.sh
+++
@@ -1,6 +0,0 @@
-echo 'number of arguments: ' $#
-echo $1 $2
-echo 'All arguments: '  $@
-a='test'
-echo $([[ $a ]] && echo $a || echo 4)
-
